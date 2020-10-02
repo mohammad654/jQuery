033350549f41c80a6bcd2bb2261363a4da5e3ebd
Mohammad <mohammad.ali.shikhi.55@gmail.com>
2020-10-02 11:39:58 +0200

    Update

---
+++ jquery/plugins.js
@@ -0,0 +1,53 @@
+
+
+
+$(document).ready(function () {
+    $("div").click(function () {
+
+            $(this).css("color","#EEE")
+    });
+});
+         //css("color","red");
+// $("input").replaceWith("<div>"+$("input")+"</div>");
+// }$("p")
+        //insertAfter($(this))
+       // css("background","red");
+         //<input type="text" name="c" value="">
+         //<input type="text" name="c" value=""> Q
+        // <button>Click to hide </button>
+        /*
+
+
+            $("button").click(function () {
+                $("<div> This Is Create</div> ").insertAfter($(this));
+               // $("div").hide();
+            });
+            $("body").delegate(".mo","click",function () {
+               $('<input type="file"><span> Choose +</span><br>').insertAfter($(this));
+            });
+
+        <section>
+    <P id="p">This Is P1</P>
+    <P>This Is P2</P>
+    <P>This Is P3</P>
+    <P >This Is P4</P>
+</section>
+
+    <div >Maandag
+    <p>Mo</p></div><br>
+    <div>Dinsdag</div><br>
+    <div>Woensdag</div><br>
+    <div>Donderdag</div><br>
+    <div>Vrijdag</div><br>
+
+      $(document).ready(function () {
+            $("div").bind({
+                click:function () {$(this).text("Mo");} ,
+
+                   dblclick:function () {$(this).text("double"); },
+                mouseenter:function () {$(this).css("color","red");},
+                mouseleave:function () {$(this).css("color","#00F");}
+                });
+        });
+
+         */
